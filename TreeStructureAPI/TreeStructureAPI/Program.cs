919d30f33fdb9be6b2500930b6f55d31e911ad29
Kacper Fila <kacperfila26@gmail.com>
2023-05-31 18:33:13 +0200

    Basic CRUD add
    -no validation implement

--- TreeStructureAPI/TreeStructureAPI/Program.cs
+++ TreeStructureAPI/TreeStructureAPI/Program.cs
@@ -21,6 +21,36 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/api/item", async (IItemService itemService, Item item) =>
+{
+    var result = await itemService.CreateItem(item);
+    return result is not null ? Results.Created($"/api/item/{result}", result) : Results.BadRequest();
+});
+
+app.MapGet("/api/item", async (IItemService itemService) =>
+{
+    var result = await itemService.GetAllItems();
+    return result.Any() ? Results.Ok(result) : Results.NotFound();
+
+});
+
+app.MapGet("/api/item/{id:Guid}", async (IItemService itemService, Guid id) =>
+{
+    var result = await itemService.GetItemById(id);
+    return result is not null ? Results.Ok(result) : Results.NotFound();
+
+});
+
+app.MapDelete("api/item/{id:guid}", async (IItemService itemService, Guid id) =>
+{
+    var result = await itemService.DeleteItem(id);
+    return result ? Results.NoContent() : Results.NotFound();
+});
+
+app.MapPost("api/item/{id:guid}", async (IItemService itemService, Guid id, Item item) =>
+{
+    var result = await itemService.UpdateItem(id, item);
+    return result ? Results.NoContent() : Results.NotFound();
+});
 
 app.Run();
